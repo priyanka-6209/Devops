032d5bda2f1874158f443cde7f7d4a0a8e947868
Priyanka Priyadarshini1 <1132571@TCS.com>
2019-02-13 11:17:13 +0530

    First commit

---
+++ src/main/java/com/devops/pirates/PiratesApplication.java
@@ -0,0 +1,14 @@
+package com.devops.pirates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/devops/pirates/PiratesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devops.pirates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PiratesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
